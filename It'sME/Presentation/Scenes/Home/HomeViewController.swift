16fd96984a10acebdaf31a6962b19e06850452ab
Jaewon Yun <woin2ee@gmail.com>
2022-11-09 18:26:08 +0900

    :sparkles: (develop) ViewController - ViewModel 관계 구성
    ViewModel 이 준수해야할 ViewModelType 프로토콜 정의
    HomeViewModel 생성
    프로젝트 그룹화

--- It'sME/Presentation/Scenes/Home/HomeViewController.swift
+++ It'sME/Presentation/Scenes/Home/HomeViewController.swift
@@ -9,9 +9,16 @@ import UIKit
 
 final class HomeViewController: UIViewController {
     
+    private let viewModel: HomeViewModel = .init()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = .white
+    }
+    
+    func bindViewModel() {
+        let input = HomeViewModel.Input()
+        let output = viewModel.transform(input: input)
     }
 }
---
+++ It'sME/Presentation/Scenes/Home/HomeViewModel.swift
@@ -0,0 +1,23 @@
+//
+//  HomeViewModel.swift
+//  It'sME
+//
+//  Created by Jaewon Yun on 2022/11/09.
+//
+
+import Foundation
+
+final class HomeViewModel: ViewModelType {
+    
+    struct Input {
+        
+    }
+    
+    struct Output {
+        
+    }
+    
+    func transform(input: Input) -> Output {
+        return .init()
+    }
+}
