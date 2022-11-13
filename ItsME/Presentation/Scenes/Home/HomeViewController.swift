f6b2d6db2bd7ed7bb05b8fdc8f98397a1840fb7a
Jaewon Yun <woin2ee@gmail.com>
2022-11-13 22:27:51 +0900

    :sparkles: (feature/home) 홈화면 viewWillAppear 시, 유저 정보를 가져오도록 구현

--- ItsME/Presentation/Scenes/Home/HomeViewController.swift
+++ ItsME/Presentation/Scenes/Home/HomeViewController.swift
@@ -6,19 +6,42 @@
 //
 
 import UIKit
+import RxSwift
 
 final class HomeViewController: UIViewController {
+    
+    private var disposeBag: DisposeBag = .init()
     
     private let viewModel: HomeViewModel = .init()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        configureAppearance()
+        bindViewModel()
+    }
+}
+
+// MARK: - Private functions
+
+private extension HomeViewController {
+    
+    func configureAppearance() {
         self.view.backgroundColor = .white
     }
     
     func bindViewModel() {
-        let input = HomeViewModel.Input()
+        let viewWillAppear = self.rx.sentMessage(#selector(self.viewWillAppear(_:)))
+            .map { _ in }
+            .asSignal(onErrorSignalWith: .empty())
+        
+        let input = HomeViewModel.Input(viewWillAppear: viewWillAppear)
         let output = viewModel.transform(input: input)
+        
+        output.userInfo
+            .drive(onNext: { userInfo in
+                // input 에 대한 output 으로 userInfo 를 받았을 때 수행할 작업 정의
+                print(userInfo)
+            })
+            .disposed(by: disposeBag)
     }
 }
