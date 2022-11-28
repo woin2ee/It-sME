77f513695a0be10bc61d5e054cf9433509851aee
blooper20 <yjw127588@gmail.com>
2022-11-28 16:30:44 +0900

    :construction: (develop/feature/HomeView) #28 페이지 고정 성공(대신 고정값을 넣어서 성공한것임으로 동적 계산 필요)

--- ItsME/Presentation/Scenes/Home/HomeViewController.swift
+++ ItsME/Presentation/Scenes/Home/HomeViewController.swift
@@ -40,6 +40,7 @@ final class HomeViewController: UIViewController, UIScrollViewDelegate {
     
     private var cardScrollView = UIScrollView()
     
+    private lazy var pageController = UIPageControl()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -51,6 +52,40 @@ final class HomeViewController: UIViewController, UIScrollViewDelegate {
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         profileImageView.circular()
+    }
+    
+    func scrollViewWillEndDragging(_ scrollView: UIScrollView, withVelocity velocity: CGPoint, targetContentOffset: UnsafeMutablePointer<CGPoint>) {
+        if let pageOffset = ScrollPageController().pageOffset(
+            for: scrollView.contentOffset.x,
+            velocity: velocity.x,
+            in: pageOffsets(in: scrollView)
+        ) {
+            print(targetContentOffset.pointee.x,"askjdhfhㄴga")
+            targetContentOffset.pointee.x = pageOffset
+            print(targetContentOffset.pointee.x,"askjdhfhga")
+        }
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+            if let pageFraction = ScrollPageController().pageFraction(
+                for: scrollView.contentOffset.x,
+                in: pageOffsets(in: scrollView)
+            ) {
+                let pageControl: UIPageControl = pageController
+                pageControl.currentPage = Int(round(pageFraction))
+            }
+        }
+    
+    private func pageOffsets(in scrollView: UIScrollView) -> [CGFloat] {
+        let pageWidth = scrollView.bounds.width
+        - scrollView.adjustedContentInset.left
+        - scrollView.adjustedContentInset.right
+        let numberOfPages = Int(ceil(scrollView.contentSize.width / pageWidth))
+        print(pageWidth, numberOfPages,scrollView.contentSize.width,"!@#!@#!@#!@#!@#!@#!@#!@#!@#!@#!@#!@#!@#!@!@!@#!@!!@#!@#!@#!@#!@#!@#!@#!@#")
+        let test = [CGFloat(0),CGFloat(280),CGFloat(560)]
+        print(test)
+//        return (0..<numberOfPages).map { CGFloat($0) * pageWidth - scrollView.adjustedContentInset.left }
+        return test
     }
 }
 
@@ -84,6 +119,7 @@ private extension HomeViewController {
         self.view.addSubview(vStackLayout)
         cardScrollView.addSubview(hStackLayout)
         self.view.addSubview(cardScrollView)
+        self.view.addSubview(pageController)
         
         editProfileButton.backgroundColor = .mainColor
         editProfileButton.setTitle("프로필 수정", for: .normal)
@@ -110,15 +146,19 @@ private extension HomeViewController {
         hStackLayout.alignment = .fill
         hStackLayout.spacing = 30
         hStackLayout.isLayoutMarginsRelativeArrangement = true
-        hStackLayout.layoutMargins.left = 100
-        hStackLayout.layoutMargins.right = 100
+        hStackLayout.layoutMargins.right = 70
+        hStackLayout.layoutMargins.left = hStackLayout.layoutMargins.right
         
         cardScrollView.delegate = self
         cardScrollView.isScrollEnabled = true
-        cardScrollView.isPagingEnabled = true
         cardScrollView.alwaysBounceHorizontal = true
         cardScrollView.layoutMargins = .zero
         cardScrollView.showsHorizontalScrollIndicator = false
+        
+        pageController.hidesForSinglePage = true
+        pageController.pageIndicatorTintColor = .gray
+        pageController.currentPageIndicatorTintColor = .black
+        pageController.backgroundColor = .mainColor
         
         editProfileButton.snp.makeConstraints { make in
             make.width.equalTo(100)
@@ -142,7 +182,7 @@ private extension HomeViewController {
         
         hStackLayout.snp.makeConstraints{ make in
             make.height.left.right.equalToSuperview()
-            make.width.equalToSuperview().multipliedBy(hStackLayout.arrangedSubviews.count)
+            make.width.equalTo(950)
         }
         
         cardScrollView.snp.makeConstraints{ make in
@@ -151,19 +191,76 @@ private extension HomeViewController {
             make.width.equalToSuperview()
         }
         
+        pageController.snp.makeConstraints{ make in
+            make.top.equalTo(cardScrollView.snp.bottom).offset(30)
+            make.height.equalTo(30)
+            make.width.equalTo(50)
+            make.centerX.equalTo(self.view.safeAreaLayoutGuide)
+        }
     }
 }
+
+struct ScrollPageController {
+    
+    /// Computes page offset from page offsets array for given scroll offset and velocity
+    ///
+    /// - Parameters:
+    ///   - offset: current scroll offset
+    ///   - velocity: current scroll velocity
+    ///   - pageOffsets: page offsets array
+    /// - Returns: target page offset from array or nil if no page offets provided
+    func pageOffset(for offset: CGFloat, velocity: CGFloat, in pageOffsets: [CGFloat]) -> CGFloat? {
+        let pages = pageOffsets.enumerated().reduce([Int: CGFloat]()) {
+            var dict = $0
+            dict[$1.0] = $1.1
+            return dict
+        }
+        guard let page = pages.min(by: { abs($0.1 - offset) < abs($1.1 - offset) }) else {
+            return nil
+        }
+        if abs(velocity) < 0.2 {
+            return page.value
+        }
+        if velocity < 0 {
+            return pages[pageOffsets.index(before: page.key)] ?? page.value
+        }
+        print(pages[pageOffsets.index(after: page.key)] ?? page.value,"왜")
+        return pages[pageOffsets.index(after: page.key)] ?? page.value
+    }
+    
+    /// Cumputes page fraction from page offsets array for given scroll offset
+    ///
+    /// - Parameters:
+    ///   - offset: current scroll offset
+    ///   - pageOffsets: page offsets array
+    /// - Returns: current page fraction in range from 0 to number of pages or nil if no page offets provided
+    func pageFraction(for offset: CGFloat, in pageOffsets: [CGFloat]) -> CGFloat? {
+        let pages = pageOffsets.sorted().enumerated()
+        if let index = pages.first(where: { $0.1 == offset })?.0 {
+            return CGFloat(index)
+        }
+        guard let nextOffset = pages.first(where: { $0.1 >= offset })?.1 else {
+            return pages.map { $0.0 }.last.map { CGFloat($0) }
+        }
+        guard let (prevIdx, prevOffset) = pages.reversed().first(where: { $0.1 <= offset }) else {
+            return pages.map { $0.0 }.first.map { CGFloat($0) }
+        }
+        return CGFloat(prevIdx) + (offset - prevOffset) / (nextOffset - prevOffset)
+    }
+    
+}
+
 
 //MARK: - for canvas
 import SwiftUI
 
 struct ViewControllerRepresentable: UIViewControllerRepresentable {
     typealias UIViewControllerType = HomeViewController
-
+    
     func makeUIViewController(context: Context) -> HomeViewController {
         return HomeViewController()
     }
-
+    
     func updateUIViewController(_ uiViewController: HomeViewController, context: Context) {
     }
 }
