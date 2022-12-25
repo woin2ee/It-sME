e6c98b1f61f0a86e2851d8f92750f41c7cce94f1
Jaewon Yun <woin2ee@gmail.com>
2022-12-25 19:40:49 +0900

    :recycle: (fix) 프로퍼티를 함수보다 위로 선언

--- ItsME/Presentation/Common/Component/ProfileInfoComponent.swift
+++ ItsME/Presentation/Common/Component/ProfileInfoComponent.swift
@@ -12,15 +12,6 @@ import SnapKit
 class ProfileInfoComponent: UIStackView {
     var userInfoItem: UserInfoItem
     
-    init(userInfoItem: UserInfoItem) {
-        self.userInfoItem = userInfoItem
-        super.init(frame: .zero)
-    }
-    
-    required init(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
     private lazy var icon: UILabel = {
         let label = UILabel()
         label.text = userInfoItem.icon.toEmoji
@@ -28,7 +19,6 @@ class ProfileInfoComponent: UIStackView {
         label.textAlignment = .center
         return label
     }()
-    
     
     private lazy var contents: UILabel = {
         let label = UILabel()
@@ -39,6 +29,15 @@ class ProfileInfoComponent: UIStackView {
         label.numberOfLines = 2
         return label
     }()
+    
+    init(userInfoItem: UserInfoItem) {
+        self.userInfoItem = userInfoItem
+        super.init(frame: .zero)
+    }
+    
+    required init(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
     
     override func layoutSubviews() {
         self.addArrangedSubview(icon)
