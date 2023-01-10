a14ae72bbb042170b48eed2d697ab63c041c231b
Jaewon Yun <woin2ee@gmail.com>
2023-01-10 12:39:18 +0900

    :construction: (feature/edit-profile) UserInfoItem 수정에 사용할 ViewController 파일 생성

---
+++ ItsME/Presentation/Scenes/EditProfile/BirthdayEditing/BirthdayEditingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  BirthdayEditingViewController.swift
+//  ItsME
+//
+//  Created by Jaewon Yun on 2023/01/10.
+//
+
+import UIKit
+
+final class BirthdayEditingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
---
+++ ItsME/Presentation/Scenes/EditProfile/OtherItemEditing/OtherItemEditingViewController.swift
@@ -0,0 +1,15 @@
+//
+//  OtherItemEditingViewController.swift
+//  ItsME
+//
+//  Created by Jaewon Yun on 2023/01/10.
+//
+
+import UIKit
+
+final class OtherItemEditingViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
