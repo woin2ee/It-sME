6818022d2c5a38e7d29664def581d4c6f8156865
Jaewon Yun <woin2ee@gmail.com>
2023-02-01 15:59:12 +0900

    :construction: (feature/edit-profile) 완료 버튼 추가

--- ItsME/Presentation/Scenes/EditProfile/OtherItemEditing/OtherItemEditingViewController.swift
+++ ItsME/Presentation/Scenes/EditProfile/OtherItemEditing/OtherItemEditingViewController.swift
@@ -5,12 +5,33 @@
 //  Created by Jaewon Yun on 2023/01/10.
 //
 
+import SnapKit
+import Then
 import UIKit
 
 final class OtherItemEditingViewController: UIViewController {
     
+    
+    
+    private lazy var completeButton: UIBarButtonItem = .init(title: "완료")
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         self.view.backgroundColor = .systemBackground
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        configureNavigationBar()
+    }
+}
+
+// MARK: - Private Functions
+
+private extension OtherItemEditingViewController {
+    
+    func configureNavigationBar() {
+        self.navigationItem.title = "새 항목"
+        self.navigationItem.rightBarButtonItem = completeButton
     }
 }
