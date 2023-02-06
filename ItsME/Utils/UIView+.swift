30af845f5f00f3c45e05276673bd0465d371daa1
Jaewon Yun <woin2ee@gmail.com>
2023-02-06 22:39:26 +0900

    :sparkles: (feature/edit-profile) View 하단에 테두리를 추가하는 함수 추가

--- ItsME/Utils/UIView+.swift
+++ ItsME/Utils/UIView+.swift
@@ -19,4 +19,25 @@ extension UIView {
         self.layer.borderColor = bordercolor
         self.layer.borderWidth = borderwidth
     }
+    
+    /// 뷰 하단에 테두리를 추가하고 해당 레이어를 반환합니다.
+    ///
+    ///  - Parameters:
+    ///     - width: 테두리의 두께
+    ///     - color: 테두리의 색깔
+    ///     - offset: 뷰 하단을 기준으로 얼마나 떨어져 있는지 나타내는 offset 값
+    func addBottomBorder(width: CGFloat = 1.0, color: UIColor = .systemGray4, offset: CGFloat = 0) -> CALayer {
+        let borderLayer: CALayer = .init()
+        let margin: CGFloat = 4.0
+        borderLayer.frame = .init(
+            x: margin,
+            y: self.bounds.height + offset,
+            width: self.bounds.width - margin * 2,
+            height: width
+        )
+        borderLayer.backgroundColor = color.cgColor
+        self.layer.addSublayer(borderLayer)
+        
+        return borderLayer
+    }
 }
