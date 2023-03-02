bc952be160375a7b14463c668bb651d57aa6c0d8
Jaewon Yun <woin2ee@gmail.com>
2023-03-02 17:02:39 +0900

    :zap: (feature/edit-profile) UITest 툴 이용해서 테스트하기 편한 환경 구성

--- ItsMEUITests/ItsMEUITests.swift
+++ ItsMEUITests/ItsMEUITests.swift
@@ -9,33 +9,44 @@ import XCTest
 
 final class ItsMEUITests: XCTestCase {
 
+    var app: XCUIApplication!
+    
     override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
+        app = XCUIApplication()
+        
         continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // UI tests must launch the application that they test.
-        let app = XCUIApplication()
-        app.launch()
-
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
+        
+        addUIInterruptionMonitor(withDescription: "Login Alert") { alert in
+            alert.buttons["계속"].tap()
+            return true
         }
+    }
+    
+    func test_프로필수정화면_이동() {
+        프로필수정화면_이동()
+        대기()
+    }
+    
+    func test_학력정보_수정화면_이동() {
+        프로필수정화면_이동()
+        
+        let scrollViewsQuery = app.scrollViews
+        scrollViewsQuery.otherElements.tables/*@START_MENU_TOKEN@*/.staticTexts["자연계"]/*[[".cells.staticTexts[\"자연계\"]",".staticTexts[\"자연계\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+        
+        대기()
+    }
+    
+    private func 프로필수정화면_이동() {
+        app.launch()
+        
+        app.buttons["kakao login large"].tap()
+        app.tap() // 앱 상태 active 로 전환
+        
+        app/*@START_MENU_TOKEN@*/.webViews.webViews.webViews.buttons["계속하기"]/*[[".otherElements[\"BrowserView?IsPageLoaded=true&WebViewProcessID=12441\"].webViews.webViews.webViews",".otherElements[\"카카오계정으로 로그인\"].buttons[\"계속하기\"]",".buttons[\"계속하기\"]",".webViews.webViews.webViews"],[[[-1,3,1],[-1,0,1]],[[-1,2],[-1,1]]],[0,0]]@END_MENU_TOKEN@*/.tap()
+        app/*@START_MENU_TOKEN@*/.staticTexts["프로필 수정"]/*[[".buttons[\"프로필 수정\"].staticTexts[\"프로필 수정\"]",".staticTexts[\"프로필 수정\"]"],[[[-1,1],[-1,0]]],[0]]@END_MENU_TOKEN@*/.tap()
+    }
+    
+    private func 대기() {
+        _ = app.wait(for: .notRunning, timeout: .infinity)
     }
 }
