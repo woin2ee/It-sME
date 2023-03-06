6a2fa7ee8129a9efa886fefec56d0204ef984579
Jaewon Yun <woin2ee@gmail.com>
2023-03-06 16:00:15 +0900

    :sparkles: :white_check_mark: (feature/common) 숫자 앞에 0을 붙여주는 Extension 함수 정의 + 테스트 케이스 작성

---
+++ ItsME/Utils/Extensions/Int+.swift
@@ -0,0 +1,29 @@
+//
+//  Int+.swift
+//  ItsME
+//
+//  Created by Jaewon Yun on 2023/03/06.
+//
+
+import Foundation
+
+extension Int {
+    
+    /**
+    주어진 숫자에 대해 `Leading zero` 방식을 적용한 문자열을 반환합니다.
+    
+    이미 주어진 숫자의 자릿수가 파라미터로 전달한 `digit` 보다 크다면 주어진 숫자를 그대로 반환합니다.
+    
+    - Parameter digit: 표현할 자릿수의 개수입니다.
+    - Returns: `Leading zero` 방식이 적용된 문자열
+    */
+    func toLeadingZero(digit: UInt) -> String {
+        let absNumString = String(abs(self))
+        if absNumString.count >= digit {
+            return self >= 0 ? absNumString : "-\(absNumString)"
+        } else {
+            let leadingZeroString = String(repeating: "0", count: Int(digit) - absNumString.count) + absNumString
+            return self >= 0 ? leadingZeroString : "-\(leadingZeroString)"
+        }
+    }
+}
