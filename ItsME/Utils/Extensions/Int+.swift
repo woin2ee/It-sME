5c9b59fa4f1ee1196dc5444c0010cc9896a644ea
Jaewon Yun <woin2ee@gmail.com>
2023-03-06 16:19:23 +0900

    :bulb: (feature/common) 주석에 함수 사용 예시 추가

--- ItsME/Utils/Extensions/Int+.swift
+++ ItsME/Utils/Extensions/Int+.swift
@@ -14,6 +14,9 @@ extension Int {
     
     이미 주어진 숫자의 자릿수가 파라미터로 전달한 `digit` 보다 크다면 주어진 숫자를 그대로 반환합니다.
     
+        let leadingZeroString = 3.toLeadingZero(digit: 2)
+        // `leadingZeroString` == "03"
+    
     - Parameter digit: 표현할 자릿수의 개수입니다.
     - Returns: `Leading zero` 방식이 적용된 문자열
     */
