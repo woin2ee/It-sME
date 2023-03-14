0824f36f22e093f484397b0e93225b0d5c7b0374
Jaewon Yun <woin2ee@gmail.com>
2023-03-14 23:00:11 +0900

    :sparkles: (feature/common) OSStatus 를 문자열로 변환하는 computed property 정의

---
+++ ItsME/Utils/Extensions/OSStatus+ToReadableString.swift
@@ -0,0 +1,16 @@
+//
+//  OSStatus+ToReadableString.swift
+//  ItsME
+//
+//  Created by Jaewon Yun on 2023/03/14.
+//
+
+import Foundation
+
+extension OSStatus {
+    
+    /// `OSStatus` 코드를 읽을 수 있는 문자열로 만들어 반환합니다.
+    var toReadableString: String? {
+        return SecCopyErrorMessageString(self, nil) as? String
+    }
+}
