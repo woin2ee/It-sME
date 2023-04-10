d977f39c86f06d09833d931c8335fa0a688ee0fb
Jaewon Yun <woin2ee@gmail.com>
2023-04-10 23:44:03 +0900

    :bulb: (develop) Reactive Extension 에 대한 주석 추가

--- ItsME/Utils/Extensions/UIViewController+presentConfirmAlert.swift
+++ ItsME/Utils/Extensions/UIViewController+presentConfirmAlert.swift
@@ -27,14 +27,14 @@ extension UIViewController {
 
 extension Reactive where Base: UIViewController {
     
-    /// <#Description#>
+    /// `UIAlertController` 를 띄우면서 확인 버튼을 선택했을 때만 이벤트를 방출하는 `Reactive wrapper` 를 반환합니다.
     /// - Parameters:
-    ///   - title: <#title description#>
-    ///   - message: <#message description#>
-    ///   - cancelAction: <#cancelAction description#>
+    ///   - title: 화면에 띄울 `UIAlertController` 의 `title`.
+    ///   - message: 화면에 띄울 `UIAlertController` 의 `message`.
+    ///   - cancelAction: 취소 버튼에 해당하는 `UIAlertAction` 객체입니다. 기본 `title` 은 "아니오" 로 설정되어있습니다.
     ///   - okAction: 확인 버튼에 해당하는 `UIAlertAction` 객체입니다. 이 파라미터로 넘긴 객체의 `handler` 는 실행되지 않습니다. 대신 `subscribe` 를 이용하여 `handler` 에 해당하는 작업을 실행하세요.
-    ///   - animated: <#animated description#>
-    /// - Returns: <#description#>
+    ///   - animated: `AlertController` 를 화면에 띄울 때 애니메이션을 적용할지에 대한 여부입니다.
+    /// - Returns: Reactive wrapper for positive action choices.
     func presentConfirmAlert(
         title: String,
         message: String? = nil,
