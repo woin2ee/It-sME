32dc2b6a1ea1fbbc7343932551b9279ceeb5d433
Jaewon Yun <woin2ee@gmail.com>
2023-04-27 15:12:03 +0900

    :sparkles: (ME-92) 편하게 TeamID 를 가져올 수 있는 Extension 추가

--- ItsMETests/ItsMETests.swift
+++ ItsMETests/ItsMETests.swift
@@ -9,28 +9,16 @@ import XCTest
 @testable import ItsME
 
 final class ItsMETests: XCTestCase {
-
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+    
+    func testGetTeamID() {
+        // Arrange
+        let teamIDForInfoDictionary: String
+        let currentSignedTeamID = "2AVC8X7323"
+        
+        // Act
+        teamIDForInfoDictionary = Bundle.main.teamID
+        
+        // Assert
+        XCTAssertEqual(teamIDForInfoDictionary, currentSignedTeamID)
     }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-        // Any test you write for XCTest can be annotated as throws and async.
-        // Mark your test throws to produce an unexpected failure when your test encounters an uncaught error.
-        // Mark your test async to allow awaiting for asynchronous code to complete. Check the results with assertions afterwards.
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
